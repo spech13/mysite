5310b678be180610529d6cc4efe442abdc99d3ab
spech13 <es-popov2@yandex.ru>
2023-05-25 09:46:06 +0300

    [dev] add django app polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2@%dboac3tykg(+y53=#*!0zl)2bb4vv(l-f-wga^0^qm36%(9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
