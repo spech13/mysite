63b58f217e687d85fba7a53249cbbd7b02357295
spech13 <es-popov2@yandex.ru>
2023-06-17 18:52:22 +0300

    [dev] modify views detail, results, votes and add results.html template

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from polls.models import Question
+from polls.models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -14,7 +15,17 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(requests, question_id):
-    return HttpResponse("You're looking at the results of question %s" %question_id)
+    question = get_object_or_404(Question, id=question_id)
+    return render(requests, "polls/results.html", {"question":question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" %question_id)
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {"question":question, "error_message":"You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
